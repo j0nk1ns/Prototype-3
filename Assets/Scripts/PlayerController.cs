ebef7f2cda1e9625098c6a0aa743f7ff7a615b03
j0nk1ns <97742736+j0nk1ns@users.noreply.github.com>
2022-11-07 11:55:25 -0500

    Jump Force

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody PlayerRb; 
+    public float jumpForce; 
+    public float gravityModifier;
+    public  bool isOnGround;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+    }    
+     
+    // Update is called once per frame
+    void Update()
+    {
+     if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+
+        {
+          PlayerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);   
+          isOnGround = false;
+       
+        }
+    }    
+        
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround  = true; 
+    }
+
+
+} 
