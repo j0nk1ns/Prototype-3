ef2eff640f6be9fdf553af0a46bcbec7eb73f476
j0nk1ns <97742736+j0nk1ns@users.noreply.github.com>
2022-11-07 12:18:25 -0500

    Jump Force

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+
+    public GameObject obstaclePrefeb;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObastacle", startDelay, repeatRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnObastacle()
+
+    {
+        Instantiate(obstaclePrefeb, spawnPos, obstaclePrefeb.transform.rotation);
+    }
+
+}
